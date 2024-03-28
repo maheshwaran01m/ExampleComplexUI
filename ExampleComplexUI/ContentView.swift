d2a4232ddd31f7a9ff0a366985aa4e27941dc675
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-03-28 11:45:43 +0530

    Initial Commit

---
+++ ExampleComplexUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleComplexUI
+//
+//  Created by MAHESHWARAN on 28/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExampleComplexUI/ExampleComplexUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleComplexUIApp.swift
+//  ExampleComplexUI
+//
+//  Created by MAHESHWARAN on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleComplexUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
