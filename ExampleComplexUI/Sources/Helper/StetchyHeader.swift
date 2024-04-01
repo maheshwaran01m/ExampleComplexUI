4ebc6982a942776dea1502b4b42a703fec160957
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-04-01 06:22:55 +0530

    Source  - Updated playlist view

---
+++ ExampleComplexUI/Sources/Helper/StetchyHeader.swift
@@ -0,0 +1,18 @@
+//
+//  StetchyHeader.swift
+//  ExampleComplexUI
+//
+//  Created by MAHESHWARAN on 31/03/24.
+//
+
+import SwiftUI
+
+struct StetchyHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StetchyHeader()
+}
---
+++ ExampleComplexUI/Sources/Views/Spotify/Components/SpotifyPlaylistHeaderCell.swift
@@ -0,0 +1,18 @@
+//
+//  SpotifyPlaylistHeaderCell.swift
+//  ExampleComplexUI
+//
+//  Created by MAHESHWARAN on 31/03/24.
+//
+
+import SwiftUI
+
+struct SpotifyPlaylistHeaderCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotifyPlaylistHeaderCell()
+}
