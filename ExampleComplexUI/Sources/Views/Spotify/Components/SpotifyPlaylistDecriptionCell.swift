40f5c548c6115e5a23aaeb8711b773bd33167855
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-04-01 06:47:35 +0530

    Source  - Updated detail view

--- ExampleComplexUI/Sources/Views/Spotify/Components/SpotifyPlaylistDecriptionCell.swift
+++ ExampleComplexUI/Sources/Views/Spotify/Components/SpotifyPlaylistDecriptionCell.swift
@@ -5,4 +5,173 @@
 //  Created by MAHESHWARAN on 31/03/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct SpotifyPlaylistDecriptionCell: View {
+  
+  let descriptionText: String
+  let userName: String
+  let subheadline: String
+  
+  var onAddClicked: (() -> Void)?
+  var onDownloadClicked: (() -> Void)?
+  var onShareClicked: (() -> Void)?
+  var onEllipsisClicked: (() -> Void)?
+  var onShuffleClicked: (() -> Void)?
+  var onPlayClicked: (() -> Void)?
+  
+  init(descriptionText: String = Product.preview.description,
+       userName: String = Product.preview.title,
+       subheadline: String = Product.preview.brand) {
+    
+    self.descriptionText = descriptionText
+    self.userName = userName
+    self.subheadline = subheadline
+  }
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 8) {
+      
+      Text(descriptionText)
+        .font(.caption)
+        .foregroundStyle(.spotifyLightGray)
+        .frame(maxWidth: .infinity, alignment: .leading)
+      
+      
+      detailView
+      subtitleView
+      bottomView
+    }
+    .font(.caption)
+    .fontWeight(.medium)
+    .foregroundStyle(.spotifyLightGray)
+  }
+  
+  private var detailView: some View {
+    HStack(spacing: 8) {
+      Image(systemName: "apple.logo")
+        .font(.title3)
+        .foregroundStyle(.spotifyGreen)
+      
+      Text("Made for ") +
+      Text(userName)
+        .bold()
+        .foregroundColor(.spotifyWhite)
+    }
+  }
+  
+  private var subtitleView: some View {
+    Text(subheadline)
+      .font(.caption)
+      .foregroundStyle(.spotifyLightGray)
+  }
+  
+  private var bottomView: some View {
+    HStack(spacing: .zero) {
+      
+      HStack(spacing: .zero) {
+        Image(systemName: "plus.circle")
+          .padding(8)
+          .background(Color.black.opacity(0.001))
+          .onTapGesture {
+            onAddClicked?()
+          }
+          
+        Image(systemName: "arrow.down.circle")
+          .padding(8)
+          .background(Color.black.opacity(0.001))
+          .onTapGesture {
+            onDownloadClicked?()
+          }
+        
+        Image(systemName: "square.and.arrow.up")
+          .padding(8)
+          .background(Color.black.opacity(0.001))
+          .onTapGesture {
+            onShareClicked?()
+          }
+        
+        Image(systemName: "ellipsis")
+          .padding(8)
+          .background(Color.black.opacity(0.001))
+          .onTapGesture {
+            onEllipsisClicked?()
+          }
+      }
+      .offset(x: -8)
+      .frame(maxWidth: .infinity, alignment: .leading)
+      
+      
+      HStack(spacing: 8) {
+        
+        Image(systemName: "shuffle")
+          .font(.system(size: 24))
+          .background(Color.black.opacity(0.001))
+          .onTapGesture {
+            onShareClicked?()
+          }
+        
+        Image(systemName: "play.circle.fill")
+          .font(.system(size: 46))
+          .background(Color.black.opacity(0.001))
+          .onTapGesture {
+            onEllipsisClicked?()
+          }
+      }
+      .foregroundStyle(.spotifyGreen)
+    }
+    .font(.title3)
+  }
+}
+
+extension SpotifyPlaylistDecriptionCell {
+  
+  func onAddClicked(_ action: @escaping () -> Void) -> Self {
+    var newView = self
+    newView.onAddClicked = action
+    return newView
+  }
+  
+  func onDownloadClicked(_ action: @escaping () -> Void) -> Self {
+    var newView = self
+    newView.onDownloadClicked = action
+    return newView
+  }
+  
+  func onShareClicked(_ action: @escaping () -> Void) -> Self {
+    var newView = self
+    newView.onShareClicked = action
+    return newView
+  }
+  
+  func onEllipsisClicked(_ action: @escaping () -> Void) -> Self {
+    var newView = self
+    newView.onEllipsisClicked = action
+    return newView
+  }
+  
+  func onShuffleClicked(_ action: @escaping () -> Void) -> Self {
+    var newView = self
+    newView.onShuffleClicked = action
+    return newView
+  }
+  
+  func onPlayClicked(_ action: @escaping () -> Void) -> Self {
+    var newView = self
+    newView.onPlayClicked = action
+    return newView
+  }
+}
+
+// MARK: - Preview
+
+struct SpotifyPlaylistDecriptionCell_Previews: PreviewProvider {
+  
+  static var previews: some View {
+    ZStack {
+      Color.black.ignoresSafeArea()
+      
+      SpotifyPlaylistDecriptionCell()
+    }
+  }
+}
--- ExampleComplexUI/Sources/Views/Spotify/Components/SpotifyPlaylistHeaderCell.swift
+++ ExampleComplexUI/Sources/Views/Spotify/Components/SpotifyPlaylistHeaderCell.swift
@@ -8,11 +8,64 @@
 import SwiftUI
 
 struct SpotifyPlaylistHeaderCell: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  
+  let title: String
+  let subtitle: String
+  let imageName: String
+  let shadowColor: Color
+  let height: CGFloat
+  
+  init(_ title: String = "", subtitle: String = "",
+       imageName: String = DatabaseHelper.imageURL,
+       shadowColor: Color = Color.spotifyBlack.opacity(0.8),
+       height: CGFloat = 300) {
+    
+    self.title = title
+    self.subtitle = subtitle
+    self.imageName = imageName
+    self.shadowColor = shadowColor
+    self.height = height
+  }
+  
+  var body: some View {
+    Rectangle()
+      .opacity(0)
+      .overlay { ImageLoaderView(imageName) }
+      .overlay(alignment: .bottomLeading, content: overlayView)
+      .stretchyHeader(height: height)
+  }
+  
+  private func overlayView() -> some View {
+    VStack(alignment: .leading, spacing: 8) {
+      Text(subtitle)
+        .font(.headline)
+      
+      Text(title)
+        .font(.largeTitle)
+        .fontWeight(.bold)
     }
+    .padding(16)
+    .foregroundStyle(.spotifyWhite)
+    .frame(maxWidth: .infinity, alignment: .leading)
+    .background(
+      LinearGradient(colors: [shadowColor.opacity(0), shadowColor],
+                     startPoint: .top, endPoint: .bottom)
+    )
+  }
 }
 
-#Preview {
-    SpotifyPlaylistHeaderCell()
+// MARK: - Preview
+
+struct SpotifyPlaylistHeaderCell_Previews: PreviewProvider {
+  
+  static var previews: some View {
+    ZStack {
+      Color.black.ignoresSafeArea()
+      
+      ScrollView {
+        SpotifyPlaylistHeaderCell("Some Playlist", subtitle: "Example")
+      }
+      .ignoresSafeArea(.container, edges: .top)
+    }
+  }
 }
