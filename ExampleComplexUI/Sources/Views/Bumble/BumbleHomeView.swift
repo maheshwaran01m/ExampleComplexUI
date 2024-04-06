6a975c67e51b9ffc6f51a28cce2013e6b0a0f2d1
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-04-06 08:18:00 +0530

    Source  - Updated colors

---
+++ ExampleComplexUI/Sources/Views/Bumble/BumbleHomeView.swift
@@ -0,0 +1,24 @@
+//
+//  BumbleHomeView.swift
+//  ExampleComplexUI
+//
+//  Created by MAHESHWARAN on 06/04/24.
+//
+
+import SwiftUI
+
+struct BumbleHomeView: View {
+  
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+// MARK: - Preview
+
+struct BumbleHomeView_Previews: PreviewProvider {
+  
+  static var previews: some View {
+    BumbleHomeView()
+  }
+}
