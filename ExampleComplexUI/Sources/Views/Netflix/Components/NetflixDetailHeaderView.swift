d92e2e8c7eda0eab2459ba55e35d72aa03905024
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-04-26 07:22:04 +0530

    Source  - Updated example

--- ExampleComplexUI/Sources/Views/Netflix/Components/NetflixDetailHeaderView.swift
+++ ExampleComplexUI/Sources/Views/Netflix/Components/NetflixDetailHeaderView.swift
@@ -8,11 +8,80 @@
 import SwiftUI
 
 struct NetflixDetailHeaderView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  
+  let imageName: String
+  let progress: Double
+  
+  var onAirPlayClicked: (() -> Void)?
+  var onCloseClicked: (() -> Void)?
+  
+  init(imageName: String = DatabaseHelper.imageURL,
+       progress: Double = 0.2) {
+    
+    self.imageName = imageName
+    self.progress = progress
+  }
+  
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      ImageLoaderView(imageName)
+      
+      CustomProgressBar(selection: progress, range: 0...1,
+                        backgroundColor: .netflixLightGray,
+                        foregroundColor: .netflixRed,
+                        cornerRadius: 2, height: 4)
+      .padding(.bottom, 4)
+      .animation(.linear, value: progress)
+      
+      HStack(spacing: 8) {
+        Circle()
+          .fill(.netflixDarkGray)
+          .overlay {
+            Image(systemName: "tv.badge.wifi")
+              .offset(y: 1)
+              .onTapGesture { onAirPlayClicked?() }
+          }
+          .frame(width: 36, height: 36)
+        
+        Circle()
+          .fill(.netflixDarkGray)
+          .overlay {
+            Image(systemName: "xmark")
+              .offset(y: 1)
+              .onTapGesture { onCloseClicked?() }
+          }
+          .frame(width: 36, height: 36)
+      }
+      .foregroundStyle(.netflixWhite)
+      .font(.subheadline)
+      .fontWeight(.bold)
+      .padding(8)
+      .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
     }
+    .aspectRatio(2, contentMode: .fit)
+  }
 }
 
-#Preview {
+extension NetflixDetailHeaderView {
+  
+  func onAirPlayClicked(_ action: @escaping () -> Void) -> Self {
+    var newView = self
+    newView.onAirPlayClicked = action
+    return newView
+  }
+  
+  func close(_ action: @escaping () -> Void) -> Self {
+    var newView = self
+    newView.onCloseClicked = action
+    return newView
+  }
+}
+
+// MARK: - Preview
+
+struct NetflixDetailHeaderView_Previews: PreviewProvider {
+  
+  static var previews: some View {
     NetflixDetailHeaderView()
+  }
 }
--- ExampleComplexUI/Sources/Views/Netflix/Components/NetflixDetailProductView.swift
+++ ExampleComplexUI/Sources/Views/Netflix/Components/NetflixDetailProductView.swift
@@ -15,9 +15,9 @@ struct NetflixDetailProductView: View {
   let seasonCount: Int?
   let hasClosedCaption: Bool
   let isTopTen: Int?
-  let description: String
+  let description: String?
   
-  let castText: String
+  let castText: String?
   var onPlayClicked: (() -> Void)?
   var onDownloadClicked: (() -> Void)?
   
@@ -27,8 +27,8 @@ struct NetflixDetailProductView: View {
        seasonCount: Int? = 2,
        hasClosedCaption: Bool = true,
        isTopTen: Int? = 6,
-       description: String = "Example for description",
-       castText: String = "Case: People") {
+       description: String? = "Example for description",
+       castText: String? = "Case: People") {
     self.title = title
     self.isNew = isNew
     self.yearReleased = yearReleased
@@ -40,10 +40,114 @@ struct NetflixDetailProductView: View {
   }
   
   var body: some View {
-    VStack {
+    VStack(alignment: .leading, spacing: 8) {
+      titleView
+      headerView
+      headerDetailView
+      buttonView
+      descriptionView
+    }
+    .foregroundStyle(.netflixWhite)
+  }
+  
+  private var titleView: some View {
+    Text(title)
+      .font(.headline)
+      .frame(maxWidth: .infinity, alignment: .leading)
+  }
+  
+  private var headerView: some View {
+    HStack(spacing: 8) {
+      if isNew {
+        Text("New")
+          .foregroundStyle(.green)
+      }
       
+      Text(yearReleased)
+      
+      if let seasonCount {
+        Text("\(seasonCount) Season")
+      }
+      
+      if hasClosedCaption {
+        Image(systemName: "captions.bubble")
+      }
+    }
+    .foregroundStyle(.netflixLightGray)
+  }
+
+  @ViewBuilder
+  private var headerDetailView: some View {
+    if let isTopTen {
+      HStack(spacing: 8) {
+        
+        Rectangle()
+          .fill(.netflixRed)
+          .frame(width: 28, height: 28)
+          .overlay {
+            VStack(spacing: -4) {
+              Text("TOP")
+                .fontWeight(.bold)
+                .font(.system(size: 8))
+              
+              Text("10")
+                .fontWeight(.bold)
+                .font(.system(size: 16))
+            }
+            .offset(y: 1)
+          }
+        
+        Text("\(isTopTen) in TV Shows Today")
+          .font(.headline)
+      }
     }
   }
+  
+  private var buttonView: some View {
+    VStack(spacing: 8) {
+      HStack {
+        Image(systemName: "play.fill")
+        Text("Play")
+      }
+      .frame(maxWidth: .infinity)
+      .padding(.vertical, 8)
+      .foregroundStyle(.netflixDarkGray)
+      .background(.netflixWhite)
+      .clipShape(.rect(cornerRadius: 8))
+      .onTapGesture { onPlayClicked?() }
+      
+      
+      HStack {
+        Image(systemName: "arrow.down.to.line.alt")
+        Text("Download")
+      }
+      .frame(maxWidth: .infinity)
+      .padding(.vertical, 8)
+      .foregroundStyle(.netflixWhite)
+      .background(.netflixDarkGray)
+      .clipShape(.rect(cornerRadius: 8))
+      .onTapGesture { onDownloadClicked?() }
+    }
+    .font(.callout)
+    .fontWeight(.medium)
+  }
+  
+  private var descriptionView: some View {
+    Group {
+      if let description {
+        Text(description)
+      }
+      
+      if let castText {
+        Text(castText)
+          .foregroundStyle(.netflixLightGray)
+      }
+    }
+    .font(.callout)
+    .frame(maxWidth: .infinity, alignment: .leading)
+    .multilineTextAlignment(.leading)
+  }
+
 }
 
 extension NetflixDetailProductView {
@@ -66,6 +170,10 @@ extension NetflixDetailProductView {
 struct NetflixDetailProductView_Previews: PreviewProvider {
   
   static var previews: some View {
-    NetflixDetailProductView()
+    ZStack {
+      Color.netflixBlack.ignoresSafeArea()
+      
+      NetflixDetailProductView()
+    }
   }
 }
